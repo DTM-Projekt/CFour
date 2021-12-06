cb3441b35f0154cb02fa3ea8479890717ddcb42f
Ingolf Ankert <github@ingank.net>
2021-12-06 23:33:14 +0100

    bb -> bitboards newbord -> bitboard Kommentare anpassen

--- core.py
+++ core.py
@@ -1,39 +1,38 @@
 class game:
 
-    bbs = list()     # Bitboards für zwei Spieler
-    counter = int()  # Anzahl der bis jetzt getätigten Spielzüge
-    width = int()    # Breite des Spielfeldes
-    height = int()   # Höhe des Spielfeldes
-    h1 = int()       # imaginäre Zeile über dem Spielfeld
-    h2 = int()       # imaginäre zweite Zeile über dem Spielfeld
-    size = int()     # Anzahl der Felder auf dem Gitter
-    s1 = int()       # Anzahl der Felder einschließlich der ersten imaginären Zeile
-    lowest = list()  # Die untersten freien Plätze pro Spalte
+    bitboards = list()     # Bitboards für zwei Spieler
+    counter = int()        # Anzahl der bis jetzt getätigten Spielzüge
+    width = int()          # Breite des Spielfeldes
+    height = int()         # Höhe des Spielfeldes
+    h1 = int()             # imaginäre Zeile über dem Spielfeld
+    h2 = int()             # imaginäre zweite Zeile über dem Spielfeld
+    size = int()           # Anzahl der Felder auf dem Gitter
+    s1 = int()             # Anzahl der Felder einschließlich der ersten imaginären Zeile
+    lowest = list()        # Die untersten freien Plätze pro Spalte als Bitwert
 
     def __init__(self, width, height):
-        self.bb = [0, 0]
+        self.bitboards = [0, 0]
         self.width = width
         self.height = height
         self.h1 = height + 1
         self.h2 = height + 2
         self.size = height * width
         self.s1 = self.h1 * width
-        # lowest wird mit den Werten der untersten Zeile vorgefüllt
+        # am Anfang sind die Spielfelder der Zeile '0' leer
         self.lowest = list(range(0, self.size + 1, self.h1))
 
     def make_move(self, row):
-        global counter
         x = 1 << self.lowest[row]
         side = self.counter & 1
-        self.bb[side] ^= x
+        self.bitboards[side] ^= x
         self.lowest[row] += 1
         self.counter += 1
 
-    def has_won(self, newboard):
-        diag1 = newboard & (newboard >> self.height)
-        hori = newboard & (newboard >> self.h1)
-        diag2 = newboard & (newboard >> self.h2)
-        vert = newboard & (newboard >> 1)
+    def has_won(self, bitboard):
+        diag1 = bitboard & (bitboard >> self.height)
+        hori = bitboard & (bitboard >> self.h1)
+        diag2 = bitboard & (bitboard >> self.h2)
+        vert = bitboard & (bitboard >> 1)
         a = (diag1 & (diag1 >> 2*self.height))
         b = (hori & (hori >> 2*self.h1))
         c = (diag2 & (diag2 >> 2*self.h2))
