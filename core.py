9b364e5ffabf5cf4a036422f63ff04d0ae6785bb
Ingolf Ankert <github@ingank.net>
2021-12-06 23:16:58 +0100

    Update core.py
    var lowest bekommt eine Sonderbehandlung

--- core.py
+++ core.py
@@ -1,7 +1,6 @@
 class game:
 
     bbs = list()     # Bitboards für zwei Spieler
-    lowest = list()  # Die untersten freien Plätze pro Spalte
     counter = int()  # Anzahl der bis jetzt getätigten Spielzüge
     width = int()    # Breite des Spielfeldes
     height = int()   # Höhe des Spielfeldes
@@ -9,19 +8,18 @@ class game:
     h2 = int()       # imaginäre zweite Zeile über dem Spielfeld
     size = int()     # Anzahl der Felder auf dem Gitter
     s1 = int()       # Anzahl der Felder einschließlich der ersten imaginären Zeile
+    lowest = list()  # Die untersten freien Plätze pro Spalte
 
     def __init__(self, width, height):
         self.bb = [0, 0]
-        self.lowest = []
         self.width = width
         self.height = height
         self.h1 = height + 1
         self.h2 = height + 2
         self.size = height * width
         self.s1 = self.h1 * width
-        # lowest vorfüllen
-        for x in range(0, self.size + 1, self.h1):
-            self.lowest.append(x)
+        # lowest wird mit den Werten der untersten Zeile vorgefüllt
+        self.lowest = list(range(0, self.size + 1, self.h1))
 
     def make_move(self, row):
         global counter
