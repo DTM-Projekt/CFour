ff227e5dead1efd2b06cf07e62fae4fa068ec950
Ingolf Ankert <github@ingank.net>
2021-12-10 21:21:35 +0100

    adapt main to new module code

--- CFour.py
+++ CFour.py
@@ -1,7 +1,15 @@
 from game import *
+from random import *
 
 D = data()
 C = core(D)
-IO = io(D)
+S = screen(D)
+K = keyboard()
 
-print(IO.grid(True, ~D.top))
+S.clear()
+print(S.grid(True))
+
+for i in range(5000):
+    print("Player", D.player, "ist am Zug")
+    C.switch() if C.move(choice(D.playable if D.playable else [0])) else None
+    print(S.grid(True))
