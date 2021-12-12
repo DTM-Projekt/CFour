3e3ba69c39e1e5994a11639568e983f2a9e3f433
Ingolf Ankert <github@ingank.net>
2021-12-12 14:54:23 +0100

    Grundgerüst

--- CFour.py
+++ CFour.py
@@ -1,20 +1,20 @@
 from game import *
 from random import *
 
-D = data()
-C = core(D)
-I = interface(D)
+C = core()
+I = interface(C)
 
 while(C.is_playable()):
     I.clear()
     print(I.headline())
     print(I.status())
     print(I.grid())
+    I.player_next_move()
 
 
 quit()
 
 for i in range(5000):
-    print("Player", D.player, "ist am Zug")
-    C.switch() if C.move(choice(D.playable if D.playable else [0])) else None
+    print("Player", C.player, "ist am Zug")
+    C.switch() if C.move(choice(D.playable if C.playable else [0])) else None
     print(I.grid(True))
