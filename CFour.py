8a6ab6784dfb20440ad38f62f13325b44e4b35df
Ingolf Ankert <github@ingank.net>
2021-12-12 19:23:16 +0100

    CFour.py: IMPL CHGs from 'game' modul

--- CFour.py
+++ CFour.py
@@ -1,20 +1,6 @@
 from game import *
-from random import *
+
 
 C = core()
 I = interface(C)
-
-while(C.is_playable()):
-    I.clear()
-    print(I.headline())
-    print(I.status())
-    print(I.grid())
-    I.player_next_move()
-
-
-quit()
-
-for i in range(5000):
-    print("Player", C.player, "ist am Zug")
-    C.switch() if C.move(choice(D.playable if C.playable else [0])) else None
-    print(I.grid(True))
+I.game_mode()
